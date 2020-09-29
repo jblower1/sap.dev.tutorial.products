ecbe5d9e5baec03cc1a60a5cfb9d90f7f24fa579
Joe Blower <jblower13@gmail.com>
2020-09-29 17:53:55 +0100

    New products view added, routing and Northwind odata added

---
+++ uimodule/webapp/controller/Products.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "sap/dev/tutorial/products/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("sap.dev.tutorial.products.controller.Products", {});
+});
